ba2d514e6d16e7858d4002350d75dec01f005eb9
jdthgeek <john.dennehy@jdthegeek.com>
2020-02-26 14:58:03 +0000

    removes existing babel as not requiored with next.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import React from 'react'
+// import React from 'react'
+import Link from 'next/link'
 
 export default function Index() {
   return (
     <div>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   );
